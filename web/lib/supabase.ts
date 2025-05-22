0d4937db4624935f4fc3de0566b93141c9abd670
Joseph Zheng <143029074+joz2005@users.noreply.github.com>
2025-05-22 10:48:07 -0400

    Corrected supabase.ts under lib folder in function call

--- web/lib/supabase.ts
+++ web/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createSupabaseClient } from "@/utils/supabase/clients/component";
+import { createSupabaseComponentClient } from "@/utils/supabase/clients/component";
 import { useMemo } from "react";
 
 export function useSupabase() {
-  return useMemo(() => createSupabaseClient(), []);
+  return useMemo(() => createSupabaseComponentClient(), []);
 }
