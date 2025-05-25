a08c63f4c4f3ecbe8dc542519e6bda4efa21da2f
Joseph <jozheng@unc.edu>
2025-05-25 16:13:27 -0400

    Added details to login page

--- web/components/login-signup/login-card.tsx
+++ web/components/login-signup/login-card.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SupabaseClient } from "@supabase/supabase-js";
+// import Image from "next/image";
 import { useState } from "react";
 import {
   Card,
@@ -9,6 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import { NextRouter } from "next/router";
 
 type LoginCardProps = {
@@ -32,28 +34,60 @@ export default function LoginCard({ supabase, router }: LoginCardProps) {
     }
   };
 
+  //   const google_icon =
+  //     "https://hfgwvyysughxuvomsxmq.supabase.co/storage/v1/object/public/public-images//google_logo.png";
+
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle>Login</CardTitle>
-        <CardDescription>Login to your account</CardDescription>
+        <CardTitle className="text-5xl">Welcome to Bittle!</CardTitle>
+        <CardDescription className="text-2xl">
+          Enter in your information to log in below.
+        </CardDescription>
       </CardHeader>
       <CardContent className="space-y-4">
-        <Input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button className="w-full" onClick={logIn}>
-          Login
-        </Button>
+        {/* <div className="flex justify-center">
+          <Button className="w-full flex items-center justify-center gap-2 px-6 py-6 text-lg">
+            <Image src={google_icon} alt="Google Icon" width={24} height={24} />
+            Continue with Google
+          </Button>
+        </div> */}
+
+        <div className="space-y-1">
+          <Label className="text-xl">Email</Label>
+          <Input
+            type="email"
+            placeholder="johndoe@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-6"
+          />
+        </div>
+
+        <div className="space-y-1">
+          <Label className="text-xl">Password</Label>
+          <Input
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-6"
+          />
+        </div>
+        <div className="flex flex-col gap-2 sm:flex-row sm:items-center justify-between p-4">
+          <p className="text-sm">
+            Don&apos;t have an account?{" "}
+            <a
+              href="/signup"
+              className="hover:underline font-bold text-primary"
+            >
+              Sign up
+            </a>
+          </p>
+          <Button className="px-20 py-6 text-lg" onClick={logIn}>
+            Login
+          </Button>
+        </div>
       </CardContent>
     </Card>
   );
--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -8,8 +8,8 @@ export default function LoginPage() {
   const supabase = createSupabaseComponentClient();
 
   return (
-    <div className="min-h-screen flex items-center justify-end bg-login-gradient bg-cover bg-no-repeat bg-center px-4 md:px-12 lg:px-24">
-      <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+    <div className="min-h-screen flex items-center justify-end bg-login-gradient bg-cover bg-no-repeat bg-center px-4">
+      <div className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl transition-all duration-300">
         <LoginCard supabase={supabase} router={router} />
       </div>
     </div>
