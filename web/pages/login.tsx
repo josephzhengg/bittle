103c2c2fb56493b159fb1c26cc80bcef76394172
Joseph <jozheng@unc.edu>
2025-05-25 16:52:33 -0400

    Added functionality for whenever user is authenticated, they're immediately taken to index

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import { createSupabaseComponentClient } from "@/utils/supabase/clients/component";
 import { useRouter } from "next/router";
 import LoginCard from "@/components/login-signup/login-card";
+import { useEffect } from "react";
 
 export default function LoginPage() {
   const router = useRouter();
   const supabase = createSupabaseComponentClient();
+
+  // Redirects to home page if user is already logged in
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        router.replace("/");
+      }
+    });
+  }, [router, supabase]);
 
   return (
     <div className="min-h-screen flex items-center justify-end bg-login-gradient bg-cover bg-no-repeat bg-center px-4">
--- web/pages/signup.tsx
+++ web/pages/signup.tsx
@@ -1,10 +1,20 @@
 import { useRouter } from "next/router";
 import { createSupabaseComponentClient } from "@/utils/supabase/clients/component";
 import SignupCard from "@/components/login-signup/signup-card";
+import { useEffect } from "react";
 
 export default function SignupPage() {
   const router = useRouter();
   const supabase = createSupabaseComponentClient();
+
+  // Redirects to home page if user is already logged in
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        router.replace("/");
+      }
+    });
+  }, [router, supabase]);
 
   return (
     <div className="min-h-screen flex items-center justify-end bg-login-gradient bg-cover bg-no-repeat bg-center px-4">
