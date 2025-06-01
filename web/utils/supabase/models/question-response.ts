19e9433e64e7292d65aa10bf2a4cfd9a69ae6c65
Zhi Hang Yang <zhiy@ad.unc.edu>
2025-06-01 16:35:12 -0400

    added and updated models

--- web/utils/supabase/models/question-response.ts
+++ web/utils/supabase/models/question-response.ts
@@ -4,7 +4,8 @@ export const QuestionResponse = z.object({
   id: z.string(),
   form_id: z.string(),
   question_id: z.string(),
-  free_text: z.string().nullable()
+  free_text: z.string().nullable(),
+  form_submission_id: z.string()
 });
 
 export type QuestionResponse = z.infer<typeof QuestionResponse>;
