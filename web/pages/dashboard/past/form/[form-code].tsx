aee0e00646110b73cbce53dd1b0cd2c4bc2d8565
Joseph <jozheng@unc.edu>
2025-07-10 17:28:55 -0400

    fixed routing for past forms

--- web/pages/dashboard/past/form/[form-code].tsx
+++ web/pages/dashboard/past/form/[form-code].tsx
@@ -202,9 +202,7 @@ export default function FormPage({
             <TabsList className="h-12 p-1 bg-transparent rounded-lg w-full grid grid-cols-2">
               <TabsTrigger
                 value="forms"
-                onClick={() =>
-                  router.push(`/dashboard/current/form/${formCode}`)
-                }
+                onClick={() => router.push(`/dashboard/past/form/${formCode}`)}
                 className="flex items-center gap-2 h-10 px-3 sm:px-6 rounded-md font-medium transition-all data-[state=active]:bg-background data-[state=active]:shadow-sm data-[state=active]:text-slate-800 text-slate-600 hover:text-slate-800">
                 <FileText className="w-4 h-4" />
                 <span className="hidden xs:inline">Forms</span>
@@ -212,7 +210,7 @@ export default function FormPage({
               <TabsTrigger
                 value="applicants"
                 onClick={() =>
-                  router.push(`/dashboard/current/applicants/${formCode}`)
+                  router.push(`/dashboard/past/applicants/${formCode}`)
                 }
                 className="flex items-center gap-2 h-10 px-3 sm:px-6 rounded-md font-medium transition-all data-[state=active]:bg-background data-[state=active]:shadow-sm data-[state=active]:text-slate-800 text-slate-600 hover:text-slate-800">
                 <Users className="w-4 h-4" />
