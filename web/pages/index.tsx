64c882b345b81e170830bb7320e26b08590ec86a
Joseph <jozheng@unc.edu>
2025-07-26 21:53:43 -0400

    removed unused variables for preview deployment

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,24 +1,11 @@
-import { createSupabaseComponentClient } from "@/utils/supabase/clients/component";
 import { createSupabaseServerClient } from "@/utils/supabase/clients/server-props";
-import { User } from "@supabase/supabase-js";
-import { useQuery } from "@tanstack/react-query";
-import { create } from "domain";
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { GetServerSidePropsContext } from "next";
 import { Label } from "@/components/ui/label";
 
-type HomeProps = {
-  user: User;
-};
-
-export default function Home({ user }: HomeProps) {
+export default function Home() {
   // This is the default page that displays when the app is accessed.
   // You can add dynamic paths using folders of the form [param] in the pages directory.
-  // In these folders, you can create a file called [param].tsx to handle dynamic routing.
-
-  const supabase = createSupabaseComponentClient();
-  const router = useRouter();
+  // In these folders, you can create a file called [param].tsx to handle dynamic routing
 
   return (
     <div>
