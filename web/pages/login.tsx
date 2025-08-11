28b821884aef4c90d304f2bc2b3e9aacff364bc3
Joseph <jozheng@unc.edu>
2025-08-10 21:00:28 -0400

    attempt to fix the text shown on search engines

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
-import { createSupabaseComponentClient } from "@/utils/supabase/clients/component";
-import { useRouter } from "next/router";
-import LoginCard from "@/components/login-signup/login-card";
-import LoginLayout from "@/components/layouts/login-card-layout";
+import { useEffect } from 'react';
+import { createSupabaseComponentClient } from '@/utils/supabase/clients/component';
+import { useRouter } from 'next/router';
+import LoginCard from '@/components/login-signup/login-card';
+import LoginLayout from '@/components/layouts/login-card-layout';
+import Head from 'next/head';
 
 export default function LoginPage() {
   const router = useRouter();
@@ -12,14 +13,41 @@ export default function LoginPage() {
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       if (session) {
-        router.replace("/dashboard/current");
+        router.replace('/dashboard/current');
       }
     });
   }, [router, supabase]);
 
   return (
-    <LoginLayout>
-      <LoginCard supabase={supabase} router={router} />
-    </LoginLayout>
+    <>
+      <Head>
+        <title>Login – Bittle</title>
+        <meta
+          name="description"
+          content="Log in to Bittle to manage families in your organization."
+        />
+        <meta property="og:title" content="Login – Bittle" />
+        <meta
+          property="og:description"
+          content="Log in to Bittle to manage families in your organization."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
+      <LoginLayout>
+        <LoginCard supabase={supabase} router={router} />
+      </LoginLayout>
+    </>
   );
 }
